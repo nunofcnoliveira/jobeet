b2fb78c4b95580bb4614ad83b77a55c221723392
Nuno Oliveira <nuno.oliveira@cleverti.com>
2015-11-17 18:47:40 +0000

    Day 1

---
+++ README.md
@@ -0,0 +1,4 @@
+jobeet
+======
+
+A Symfony project created on November 17, 2015, 4:32 pm.
---
+++ src/Nuno/JobeetBundle/NunoJobeetBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Nuno\JobeetBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class NunoJobeetBundle extends Bundle
+{
+}
